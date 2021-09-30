6bd94ed16a0d1058ce435c890bf514f7444f7d83
tabi <socialowl@tutanota.com>
2021-09-30 20:03:30 +0200

    add register / login and connect to db

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request
+from functools import wraps
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-from werkzeug.security import generate_password_hash
+from werkzeug.exceptions import abort
+from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -28,17 +32,26 @@ class NewTask(FlaskForm):
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    register = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    login = SubmitField("Login")
 
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tasks.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+Base = declarative_base()
 
 
 # CONFIGURE TABLES
-class User(db.Model):
+class User(UserMixin, db.Model, Base):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250))
@@ -46,7 +59,7 @@ class User(db.Model):
     task = relationship("Task", back_populates="user")
 
 
-class Task(db.Model):
+class Task(db.Model, Base):
     __tablename__ = "task"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
@@ -59,46 +72,93 @@ class Task(db.Model):
 db.create_all()
 
 
-@app.route("/", methods=["POST", "GET"])
-def home():
-    global x
-    form = NewTask()
-    register_form = UserForm()
-    if form.validate_on_submit():
-        dictionary_tasks[x] = {"name": form.task_name.data, "tag": form.tag_name.data, "date": str(form.date_time.data)}
-        print(dictionary_tasks)
-        x += 1
-        return redirect(url_for("home"))
-    elif register_form.validate_on_submit() and request.method == "POST":
-        new_user = User(
-            username=register_form.name.data,
-            password=generate_password_hash(password=request.form["password"], method='pbkdf2:sha256',
-                                            salt_length=8)
-        )
-        print("user created")
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template("index.html", form=form, tasks=dictionary_tasks, registere_form=register_form)
-
-
-@app.route("/add", methods=["POST", "GET"])
 def add_to_db():
+    global dictionary_tasks
     for key, value in dictionary_tasks.items():
         new_task = Task(
             task_name=value["name"],
-            user_id=1,
+            user_id=current_user.get_id(),
             tag=value["tag"],
             date=value["date"]
         )
         db.session.add(new_task)
     db.session.commit()
-    return redirect(url_for('home'))
+    dictionary_tasks = {}
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+def logged_user(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    global x
+    db_tasks = None
+    form = NewTask()
+    register_form = UserForm()
+    login_form = LoginForm()
+    if form.submit.data and form.validate():
+        if current_user.is_authenticated:
+            new_task = Task(
+                task_name=form.task_name.data,
+                user_id=1,
+                tag=form.tag_name.data,
+                date=str(form.date_time.data)
+            )
+            db.session.add(new_task)
+            db.session.commit()
+        else:
+            dictionary_tasks[x] = {"name": form.task_name.data, "tag": form.tag_name.data,
+                                   "date": str(form.date_time.data)}
+            x += 1
+        return redirect(url_for("home"))
+    elif login_form.login.data and login_form.validate():
+        if User.query.filter_by(username=request.form['name']).first():
+            user = User.query.filter_by(username=request.form['name']).first()
+            if check_password_hash(user.password, request.form['password']):
+                login_user(user)
+                return redirect(url_for('home'))
+            else:
+                flash("Wrong password")
+        else:
+            flash("User does not exist.")
+    elif register_form.register.data and register_form.validate():
+        new_user = User(
+            username=register_form.name.data,
+            password=generate_password_hash(password=request.form["password"], method='pbkdf2:sha256',
+                                            salt_length=8)
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))
+    if current_user.is_authenticated:
+        if dictionary_tasks:
+            add_to_db()
+        db_tasks = Task.query.all()
+    return render_template("index.html", form=form, local_tasks=dictionary_tasks, db_tasks=db_tasks,
+                           registere_form=register_form,
+                           login_form=login_form, logged_user=current_user.is_authenticated)
 
 
 @app.route("/delete/<int:task_id>", methods=["POST", "GET"])
 def delete(task_id):
-    del dictionary_tasks[task_id]
+    if current_user.is_authenticated:
+        task_to_delete = Task.query.get(task_id)
+        db.session.delete(task_to_delete)
+        db.session.commit()
+    else:
+        del dictionary_tasks[task_id]
     return redirect(url_for("home"))
 
 
